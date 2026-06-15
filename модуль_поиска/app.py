9b7f6151b53cf72efad12b3b7528bdd41f0cace8
pestraevayu-yulche <pestraevayu@gmail.com>
2026-06-15 23:38:18 +0500

    Update app.py

--- модуль_поиска/app.py
+++ модуль_поиска/app.py
@@ -703,7 +703,7 @@ def status():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    app.run(debug=False, host='0.0.0.0', port=port)
 
 
 @app.route('/api/admin_return_url')
